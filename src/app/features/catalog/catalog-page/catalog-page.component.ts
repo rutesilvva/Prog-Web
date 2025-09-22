d027dae5dee3cfebd71c46f24d112bf5b8268b8b
Vinicius <viniipvp@gmail.com>
2025-09-22 19:05:51 -0300

    loginPage

--- src/app/features/catalog/catalog-page/catalog-page.component.ts
+++ src/app/features/catalog/catalog-page/catalog-page.component.ts
@@ -34,10 +34,10 @@ export class CatalogPageComponent {
 
   products: Product[] = [
     { title: 'Kubernetes para desenvolvedores', slug: 'kubernetes-dev', price: 120, img: 'https://m.media-amazon.com/images/I/61EAJDHqiDL._UF894,1000_QL80_.jpg', category: 'devops' },
-    { title: 'Redes Linux Avançadas', slug: 'redes-linux', price: 89, img: '/assets/prod/linux-network.png', category: 'netsec' },
-    { title: 'Arquitetura de Software Moderna', slug: 'arquitetura-moderna', price: 99, img: '/assets/prod/arch-modern.png', category: 'arch' },
-    { title: 'Aprendizado de Máquina Essencial', slug: 'ml-essencial', price: 79, img: '/assets/prod/ml.png', category: 'ia-ml' },
-    { title: 'TypeScript Completo', slug: 'typescript', price: 110, img: '/assets/prod/typescript.png', category: 'prog' }
+    { title: 'Redes Linux Avançadas', slug: 'redes-linux', price: 89, img: 'https://imagens.disal.com.br/produtos/ampliada/1191128.jpg', category: 'netsec' },
+    { title: 'Arquitetura de Software Moderna', slug: 'arquitetura-moderna', price: 99, img: 'https://m.media-amazon.com/images/I/81ZE1M+bueL._UF1000,1000_QL80_.jpg', category: 'arch' },
+    { title: 'Aprendizado de Máquina Essencial', slug: 'ml-essencial', price: 79, img: 'https://m.media-amazon.com/images/I/814TXmfTKLL._UF1000,1000_QL80_.jpg', category: 'ia-ml' },
+    { title: 'TypeScript Completo', slug: 'typescript', price: 110, img: 'https://www.casadocodigo.com.br/cdn/shop/products/p_801e56cc-890d-4709-8dee-76288fc53c49_large.jpg?v=1627678101', category: 'prog' }
   ];
 
   get filteredProducts(): Product[] {
